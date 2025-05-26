956ee05b583291036132c4b1600be9fc9c12ebf2
huy034 <124760594+huy034@users.noreply.github.com>
2025-05-26 07:30:29 +0700

    ThucHanh1

---
+++ MvcMovie/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace MVCMOVIE.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
